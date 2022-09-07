f81f3d33c57b0935d55ca60ce3f3d32c0c02e86f
leechanhui1234 <yepp0517@naver.com>
2022-09-07 09:39:07 +0900

    update capture code

--- capture.js
+++ capture.js
@@ -1,9 +1,9 @@
-let camera_button = document.querySelector("#start-camera");
-let video = document.querySelector("#video");
-let click_button = document.querySelector("#click-photo");
-let canvas = document.querySelector("#canvas");
-let myImage = document.querySelector("#myImage");
-let load = document.querySelector("#load");
+const camera_button = document.querySelector("#start-camera");
+const video = document.querySelector("#video");
+const click_button = document.querySelector("#click-photo");
+const canvas = document.querySelector("#canvas");
+const myImage = document.querySelector("#myImage");
+const load = document.querySelector("#load");
 let image_data_url = '';
 
 camera_button.addEventListener('click', async function() {
